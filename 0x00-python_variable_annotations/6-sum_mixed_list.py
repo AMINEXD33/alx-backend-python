fd912c5f3f1d4319494be4b4873372dc27b14498
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-10 23:17:56 +0100

    all

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""this module contains a function that sums a list"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float | int]) -> float:
+    """
+    this function sums floats and its and return the sum
+    """
+    sum: float = 0
+    for numb in mxd_lst:
+        sum += numb
+    return sum
