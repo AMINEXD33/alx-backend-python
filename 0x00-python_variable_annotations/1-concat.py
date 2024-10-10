2dd0ab7ba7de9076ad9460226be3bba2b24e4df8
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-10 19:00:35 +0100

    all

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""this module contains a function that concats two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    a function that concats two strings
+    """
+    return str1 + str2
