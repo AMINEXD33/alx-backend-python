6ff6ea5fc33775acdbd99901abb975a1ba866a11
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-10 19:36:32 +0100

    all

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""this module has a function that returs a string from a float"""
+
+
+def to_str(n: float) -> str:
+    """
+    this function returns a string from a float
+    """
+    return (str)(n)
