4f9ae2a2542ca5d8ec1819148630f0b2f70e2259
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-10 19:07:41 +0100

    all

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import math
+
+"""this module contains a function that floors a float"""
+
+
+def floor(n: float) -> float:
+    """
+    this function floors a giving number
+    """
+    return math.floor(n)
