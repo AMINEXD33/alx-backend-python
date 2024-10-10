73e9e8ded553012423fbf1f881b11382f40c6fb6
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-10 23:41:09 +0100

    all

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,7 +1,11 @@
-# The types of the elements of the input are not know
-
+#!/usr/bin/env python3
+"""Task 100's module.
+"""
 from typing import Iterable, Sequence, Union
-def safe_first_element(lst:Iterable[Sequence]) -> Union[any, None]:
+
+
+def safe_first_element(lst: Iterable[Sequence]) -> Union[any, None]:
+    """return the first element of an array"""
     if lst:
         return lst[0]
     else:
