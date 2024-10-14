3d44a762f9c82989ec748061e399fed02664c0bc
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-14 16:29:29 +0100

    all

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""a module that has an async function :)"""
+
+import asyncio
+from asyncio import sleep as asyn_sleep
+from random import uniform as ran_uniform
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """
+    an async function that takes a max delay , waits it out
+    then return it
+    """
+    delay: float = ran_uniform(0, max_delay)
+    await asyn_sleep(delay)
+    return delay
