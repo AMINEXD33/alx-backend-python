2d7601d8f733f083086a51a350986bb48a8a0a84
AMINEXD33 <amine.gamer222@gmail.com>
2024-10-16 00:39:57 +0100

    all

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,15 +2,15 @@
 """this module contains an async function , nothing else :("""
 
 from asyncio import sleep as asyncio_sleep
-from typing import AsyncGenerator
+from typing import Generator
 from random import uniform as rand_uniform
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None]:
     """
     Yield 10 different values that are
     between 0 and 10, with a 1-second delay between each.
     """
-    for x in range(10):
+    for _ in range(10):
         await asyncio_sleep(1)
         yield rand_uniform(0, 10)
